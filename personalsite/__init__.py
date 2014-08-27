f6c3d76a541f35c4f741ca38728368f0367732a2
Rich Wareham <rjw57@cam.ac.uk>
2014-08-27 16:12:22 +0100

    generate secure flask session key on OpenShift

--- personalsite/__init__.py
+++ personalsite/__init__.py
@@ -7,8 +7,18 @@ from .google import create_oauth, app as google_app
 
 app = Flask(__name__)
 
-# TODO: make this more secure
-app.secret_key = 'development'.encode('utf8')
+# If running on OpenShift be a little more sensible about generating secret
+# data.
+if 'OPENSHIFT_DATA_DIR' in os.environ:
+    secret_key_path = os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'session.key')
+    if os.path.isfile(secret_key_path):
+        app.secret_key = open(secret_key_path, 'rb').read()
+    else:
+        import ssl
+        app.secret_key = ssl.RAND_bytes(64)
+        open(secret_key_path, 'wb').write(app.secret_key)
+else:
+    app.secret_key = 'development'.encode('utf8')
 
 # Write up OAuth
 app.config['google'] = create_oauth(app)
