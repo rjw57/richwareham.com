c60e54a1fa814d25bc5e4c887cebc55a1fbf5bd5
Rich Wareham <rjw57@cam.ac.uk>
2014-09-08 13:25:07 +0100

    __main__: add migration object

--- personalsite/__main__.py
+++ personalsite/__main__.py
@@ -1,8 +1,10 @@
 """Run a development webserver.
 """
-from personalsite import app
+from personalsite import app, db
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
