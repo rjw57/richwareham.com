6be4f9098588d72a349550334acaac495cd68dee
Rich Wareham <rjw57@cam.ac.uk>
2014-09-08 13:00:22 +0100

    manager: no longer configure app twice
    The personalsite.app variable is already configures. No need to do it
    twice.

--- personalsite/__main__.py
+++ personalsite/__main__.py
@@ -4,9 +4,6 @@ from personalsite import app
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-from .config import configure
-configure(app)
-
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.run()
