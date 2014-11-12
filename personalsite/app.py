ae5fba339835671266ac3a6699839004c00a8b15
Rich Wareham <rjw57@cam.ac.uk>
2014-11-12 10:50:06 +0000

    add academic endpoint to configuration

--- personalsite/app.py
+++ personalsite/app.py
@@ -58,7 +58,7 @@ def root():
 def toplevel_endpoint(path):
     return app.send_static_file(os.path.join(path, 'index.html'))
 
-for endpoint in ['software','publications','research','teaching','contact','cv','about','links','articles']:
+for endpoint in ['software','publications','research','teaching','contact','cv','about','links','articles','academic']:
     toplevel_endpoint = app.route('/' + endpoint, defaults={'path': endpoint})(
             toplevel_endpoint)
 
